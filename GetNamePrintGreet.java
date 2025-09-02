bd0456cb970fd30bcac9f2c82ada7544c9ec9149
vikranth1829 <vikranth1829@gmail.com>
2025-09-02 23:00:05 +0530

    First Commit

---
+++ GetNamePrintGreet.java
@@ -0,0 +1,14 @@
+package CodePractice;
+import java.util.Scanner;
+public class GetNamePrintGreet {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		
+		System.out.println("Hello, Welcome "+name+"!");
+
+	}
+
+}
