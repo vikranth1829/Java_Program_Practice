272518d48c761a452b6d6cbdbf8ce32438b63f52
vikranth1829 <vikranth1829@gmail.com>
2025-09-05 14:48:50 +0530

    first commit

---
+++ JavaTrainingSession/src/MathsProgram/AreaOfParallelogram.java
@@ -0,0 +1,15 @@
+package MathsProgram;
+import java.util.Scanner;
+public class AreaOfParallelogram {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Height of Parallelogram:");
+		double height=sc.nextDouble();
+		System.out.println("Enter the Base of Parallelogram:");
+		double base=sc.nextDouble();
+		double area=base*height;
+		System.out.printf("Area of Parallelogram is :%.2f",area);		
+	}
+}
