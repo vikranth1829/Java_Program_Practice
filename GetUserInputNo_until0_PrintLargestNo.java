219c0e1d52fc3af6466076bdab1f7ae149534cf6
vikranth1829 <vikranth1829@gmail.com>
2025-09-05 17:22:05 +0530

    First Commit

---
+++ GetUserInputNo_until0_PrintLargestNo.java
@@ -0,0 +1,19 @@
+package CodePractice;
+import java.util.Scanner;
+public class GetUserInputNo_until0_PrintLargestNo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int sum=0;
+		System.out.println("Enter the numbers:'0' to End:");
+		while(true) 
+		{
+		 int num=sc.nextInt();
+		if(num==0)
+			break;
+		sum=sum+num;
+		}
+		System.out.println(sum);
+		sc.close();
+}
+}
