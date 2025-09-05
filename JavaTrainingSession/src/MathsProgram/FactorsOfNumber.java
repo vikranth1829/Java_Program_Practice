cb355b37dc262c8b4c0b1ed0613a77ba589b0021
vikranth1829 <vikranth1829@gmail.com>
2025-09-05 15:52:51 +0530

    First Commit

---
+++ JavaTrainingSession/src/MathsProgram/FactorsOfNumber.java
@@ -0,0 +1,31 @@
+package MathsProgram;
+import java.util.Scanner;
+public class FactorsOfNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter any number:");
+		int num=sc.nextInt();
+		
+//		if(sc.hasNextInt())
+//		{
+//		 num=sc.nextInt();
+//		}else {
+//			System.out.println("Invalid Input : Enter integer:");
+//			sc.next();
+//		}
+		
+		if(num>0)
+		{
+			for(int i=1;i<=num;i++)
+			{
+				if(num%i==0)
+				{
+					System.out.println(i+" ");
+				}
+			}
+		}else {
+		System.out.println("Enter number greater than Zero");	
+	}
+}
+}
