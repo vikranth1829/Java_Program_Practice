69ea450974f26431cda9b0cefc45347f16cd10b1
vikranth1829 <vikranth1829@gmail.com>
2025-09-06 17:53:20 +0530

    first commit

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -40,7 +40,7 @@ public class FibonacciSeries {
 //		int n1=0;
 //		int n2=1;
 //		System.out.print(n1+" "+n2);
-//		for (int i=1;i<=n;i++)
+//		for (int i=3;i<=n;i++)
 //		{
 //			int n3=n1+n2;
 //			System.out.print(" "+n3);
@@ -57,7 +57,6 @@ public class FibonacciSeries {
 //		int i=3;
 //		while (i<=n)
 //		{
-//			
 //			int n3=n1+n2;
 //			System.out.print(" "+n3);
 //			n1=n2;
