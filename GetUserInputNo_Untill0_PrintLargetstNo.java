deb3d074545806b6133f9efeaafc7a63e5939b95
vikranth1829 <vikranth1829@gmail.com>
2025-09-06 14:35:07 +0530

    change

--- GetUserInputNo_Untill0_PrintLargetstNo.java
+++ GetUserInputNo_Untill0_PrintLargetstNo.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 public class GetUserInputNo_Untill0_PrintLargetstNo {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        int largest = Integer.MIN_VALUE;
-
-        System.out.println("Enter numbers (0 to end):");
-
-        while (true) {
-            int num = sc.nextInt();
-            if (num == 0) break;
-            if (num > largest) largest = num;
-        }
-
-        if (largest == Integer.MIN_VALUE) {
-            System.out.println("No numbers were entered.");
-        } else {
-            System.out.println("Largest number: " + largest);
-        }
-        sc.close();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the numbers(zero to End):");
+		int largest =Integer.MIN_VALUE;
+		int num;
+		while(true)  //for(;;)
+		{
+			num=sc.nextInt();
+			if(num==0)
+				break;
+			if(num>largest)
+				largest=num;
+		}
+		if(largest==Integer.MIN_VALUE)
+			System.out.println("No Numbers are entered!");
+		else 
+			System.out.println("Largest Number is : "+largest);
+		sc.close();
 	}
-
 }
