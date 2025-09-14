315b8cdbf9821fb79e348a6fb48a4ff95bd6582b
vikranth1829 <vikranth1829@gmail.com>
2025-09-14 23:56:17 +0530

    First commit with update

---
+++ PrimeBetweenTwoNumbers.java
@@ -0,0 +1,31 @@
+package CodePractice;
+import java.util.Scanner;
+public class PrimeBetweenTwoNumbers {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("Enter 1st Number : ");
+		 int fn=sc.nextInt();
+		 System.out.print("Enter last Number : ");
+		 int ln=sc.nextInt();
+		 
+		 for(int n=fn;n<=ln;n++)
+		 {
+			 if(n>1) 
+			 {
+				boolean b=true;
+				for(int i=2;i<n;i++)
+				{
+					if(n%2==0)
+					{
+						b=false;
+						break;
+					}
+				}
+				 if(b)
+					 System.out.print(n+" ");
+			 }
+		 }
+		 sc.close();
+	}
+}
